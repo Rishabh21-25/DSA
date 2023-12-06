c0e748550cfda9261b3302e2b886ed6a55532c33
Rishabh Shukla <96323784+Rishabh21-25@users.noreply.github.com>
2023-12-06 09:51:06 +0530

    added files

--- Tree/bfsLineByLine.cpp
+++ Tree/bfsLineByLine.cpp
@@ -56,34 +56,3 @@ int main()
     printLevel(root);
     return 0;
 }
-
-/*
-
-void printLevel(Node *root)
-{
-    if(root==NULL)
-    {
-        return;
-    }
-    queue<Node>*q;
-    q.push(root);
-    q.push(NULL);
-    while(q.size()>1)
-    {
-        Node *curr = q.front();
-        q.pop();
-        if(curr==NULL)
-        {
-            cout<<"\";
-            q.push(NULL);
-            continue;
-        }
-        cout<<curr->key<<" ";
-        if(curr->left!=NULL)
-        {
-            q.push(curr->left)
-        }
-    }
-}
-
-*/
