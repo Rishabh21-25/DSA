5fda1df5c061ab0e6cbe257b95c7fde14fb1414d
Rishabh Shukla <96323784+Rishabh21-25@users.noreply.github.com>
2023-12-07 17:55:53 +0530

    added files

---
+++ Stacks & Queue/Queue/queueArr.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+struct Queue
+{
+    int size, cap;
+    int *arr;
+
+    Queue(int c)
+    {
+        cap = c;
+        size = 0;
+        arr = new int[cap];
+    }
+
+    void enque(int x)
+    {
+        if (isFull())
+        {
+            return;
+        }
+        arr[size] = x;
+        size++;
+    }
+
+    void deque()
+    {
+        if (isEmpty())
+        {
+            return;
+        }
+        for (int i = 0; i < size - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        size--;
+    }
+
+    int getFront()
+    {
+        if (isEmpty())
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int getRear()
+    {
+        if (isEmpty())
+        {
+            return -1;
+        }
+        else
+        {
+            return (size - 1);
+        }
+    }
+
+    bool isFull()
+    {
+        return (size == cap);
+    }
+
+    bool isEmpty()
+    {
+        return (size == 0);
+    }
+};
